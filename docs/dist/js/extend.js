61405717edf2f59f7596560e5aa7f69a26482861
caoshiyuan <codesircao@126.com>
2022-10-11 17:29:58 +0800

    build:

--- docs/dist/js/extend.js
+++ docs/dist/js/extend.js
@@ -18,4 +18,12 @@ window.onload = function () {
   }
 
   window.bit_doc_verison = '1.0.3';
+
+  if (window.navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
 };
